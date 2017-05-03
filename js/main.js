cdde04c467553c7be8d13208d52f68fb47729f56
responsive309 <654943105@qq.com>
2017-05-03 02:38:51 -0400

    - tested and integrated flickr api

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 angular.module('nasa-central', ['ui.router','hSweetAlert'])
 
-.config(function($stateProvider, $urlRouterProvider, $locationProvider){  
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 
-  $stateProvider
-    .state('main', {url: '/main', templateUrl: 'partials/main.html', controller: 'mainCtrl'})
-    .state('temporary', {url: '/temporary', templateUrl: 'partials/temporary.html', controller: 'temporaryCtrl'})
-    ;
+    $stateProvider
+        .state('main', {url: '/main', templateUrl: 'partials/main.html', controller: 'mainCtrl'})
+        .state('temporary', {url: '/temporary', templateUrl: 'partials/temporary.html', controller: 'temporaryCtrl'})
+        ;
 
-    $urlRouterProvider.otherwise('/main');
+        $urlRouterProvider.otherwise('/main');
 
 })
 
@@ -19,8 +19,13 @@ angular.module('nasa-central', ['ui.router','hSweetAlert'])
 
 //controllers-----------------------------------------------------------------------------------------------------------------------------------------
 
-.controller('mainCtrl', function($scope, $stateParams, $state, sweet) {
+.controller('mainCtrl', function($scope, $stateParams, $state, sweet, spaceshipService) {
 
+    spaceshipService.getAllFlicks().then(function(res) {
+        $scope.totalFlicks = res.data.length;
+        console.log('getAllFlicks result length: ', res.data.length);
+        
+      })
 })
 
 .controller('temporaryCtrl', function($scope, $stateParams, $state, $location) {
--- js/services.js
+++ js/services.js
@@ -1,8 +1,26 @@
+'use strict';
+
 angular.module('nasa-central')
 
 .service('spaceshipService', function($http, $location){
+    var config = {
+        apiKey: "a5e95177da353f58113fd60296e1d250",
+        apiUrl: "https://api.flickr.com/services/rest/"
+    };
 
+    var offset = {
+        page: 1,
+        per_page: 20
+    };
 
+    this.getAllFlicks = function() {
+        return $http({
+            method: 'GET',
+            url: config.apiUrl + "?method=flickr.people.getPublicPhotos&api_key=" 
+                + config.apiKey + "&user_id=24662369@N07&format=json&page=" + offset.page 
+                + "&per_page=" + offset.per_page
+        });
+    };
 
 })
 ;
