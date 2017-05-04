558be1a2ef55c06654299a46e812dc030b1590bc
responsive309 <654943105@qq.com>
2017-05-04 03:03:34 -0400

    completed search by all/color, improved UI

--- app/js/main.js
+++ app/js/main.js
@@ -22,18 +22,39 @@ angular.module('nasa-central', ['ui.router', 'hSweetAlert'])
 //controllers-----------------------------------------------------------------------------------------------------------------------------------------
 
 .controller('mainCtrl', function($scope, $stateParams, $state, sweet, spaceshipService) {
-    $scope.photos = [];
 
-    spaceshipService.getPhotosByColor(6).then(function(res) {
-        $scope.photoCount = res.data.photos.total;
+    $scope.allPhotos = function(){
+        $scope.photos = [];
 
-        for(var p = 0; p < res.data.photos.photo.length; p++){
-            $scope.photos.push(res.data.photos.photo[p]);
-        }
+        spaceshipService.getAllPhotos().then(function(res) {
+            $scope.photoCount = res.data.photos.total;
 
-        console.log("$scope.photos: ", $scope.photos);
-        
-      })
+            for(var p = 0; p < res.data.photos.photo.length; p++){
+                $scope.photos.push(res.data.photos.photo[p]);
+            }
+
+            console.log("$scope.photos: ", $scope.photos);
+            
+        })
+    }
+
+    $scope.photosByColor = function(color){
+        $scope.photos = [];
+
+        spaceshipService.getPhotosByColor(color).then(function(res) {
+            $scope.photoCount = res.data.photos.total;
+
+            for(var p = 0; p < res.data.photos.photo.length; p++){
+                $scope.photos.push(res.data.photos.photo[p]);
+            }
+
+            console.log("$scope.photos: ", $scope.photos);
+            
+        })
+    }
+
+    $scope.allPhotos();
+    
 })
 
 .controller('upcomingCtrl', function($scope, $stateParams, $state, $location) {
